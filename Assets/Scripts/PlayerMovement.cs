150e4f884da41233e4d4ada94f4b15a94ab8d7bf
BuBuu1999 <blackhawk_blue@outlook.com>
2025-04-21 17:10:39 +0700

    Add Adjust Jump Codition

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -26,7 +26,7 @@ public class PlayerMovement : MonoBehaviour
         rb2d.linearVelocity = new Vector2(move * speed, rb2d.linearVelocity.y); 
 
         // Jump
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && !isJumping)
         {
             rb2d.AddForce(new Vector2 (rb2d.linearVelocity.x, jumpForce));
             Debug.Log("Jump!!");
